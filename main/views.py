bc153a31a710c0ccb5447608b5622129c293d398
Fata Aulia <fata.akhmad@ui.ac.id>
2026-09-26 15:26:39 +0700

    Tutorial 4

--- main/views.py
+++ main/views.py
@@ -210,15 +210,14 @@ def logout_user(request):
 
 
 @login_required(login_url="/login/")
-def toggle_star(request, project_id):
-    project = get_object_or_404(Experience, pk=project_id)
+@login_required(login_url="/login/")
+def toggle_star(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
 
     if request.method == "POST":
-        # Kalau akun ini sudah pernah memberi star, batalkan star-nya.
-        # Kalau belum, tambahkan star.
-        if request.user in Experience.starred_by.all():
-            project.starred_by.remove(request.user)
+        if request.user in experience.starred_by.all():
+            experience.starred_by.remove(request.user)
         else:
-            project.starred_by.add(request.user)
+            experience.starred_by.add(request.user)
 
     return redirect("main:show_experience")
